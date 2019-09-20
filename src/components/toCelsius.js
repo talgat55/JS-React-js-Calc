ecf5bb4d4bcc93a898f134632e6ecf2903f2ee16
talgat55 <lightxdesign55@gmail.com>
2019-09-20 09:01:38 +0600

    add calc

---
+++ src/components/toCelsius.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const   toCelsius = (fahrenheit)  =>{
+    return (fahrenheit - 32) * 5 / 9;
+};
+export default toCelsius;
---
+++ src/components/toFahrenheit.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const   toFahrenheit =(celsius) => {
+    return (celsius * 9 / 5) + 32;
+};
+export default toFahrenheit;
